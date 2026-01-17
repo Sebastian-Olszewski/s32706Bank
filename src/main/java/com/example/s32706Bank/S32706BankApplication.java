4bccdb16fe466f5ee77a221f279f2febc01d26d3
Sebastian Olszewski <mrsebcio019@gmail.com>
2026-01-17 10:24:24 +0100

    Initial commit

---
+++ src/main/java/com/example/s32706Bank/S32706BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s32706Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S32706BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S32706BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s32706Bank/S32706BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s32706Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S32706BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
